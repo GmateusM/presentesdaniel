faa9849e16861eb6ab4cee6c855d0e97bfb2e745
GmateusM <107721308+GmateusM@users.noreply.github.com>
2025-03-11 14:32:16 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,7 +12,7 @@ const initialGifts: Gift[] = [
   { id: 2, name: "Conjunto de azeiteiro e vinagreiro", imageUrl: "/lovable-uploads/1bf77254-dda3-4090-95d4-7df69d04acbe.png" },
   { id: 3, name: "Tábua de corte de bambu", imageUrl: "/lovable-uploads/65337925-ac5d-41c8-86cb-7a64bc98fce3.png" },
   { id: 4, name: "Frigideira antiaderente vermelha", imageUrl: "/lovable-uploads/93bcd672-7d32-4f78-873e-77e8dcc4400f.png" },
-  { id: 5, name: "Assadeira de aço inox", imageUrl: "/lovable-uploads/4e413534-13ad-43d1-a1b1-2e70b36d3861.png" },
+  { id: 5, name: "Assadeira de aço inox", imageUrl: "https://a-static.mlcdn.com.br/800x560/assadeira-retangular-inox-48-cm-tramontina/mobly/tr103ac47fxw-39998/5698305eb8d92661a71100a0b8d62a6c.jpg" },
   { id: 6, name: "Moedor de pimenta em madeira", imageUrl: "/lovable-uploads/4e413534-13ad-43d1-a1b1-2e70b36d3861.png" },
   { id: 7, name: "Conjunto de panelas turquesa (5 peças)", imageUrl: "/lovable-uploads/70d24cc3-3b5c-408d-81e2-0f482b5d9314.png" },
   { id: 8, name: "Panela de pressão azul", imageUrl: "/lovable-uploads/429ae40c-5ef0-405c-8a03-11e17efe3987.png" },
